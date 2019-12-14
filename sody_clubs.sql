d7fa1057ffdd4db938942706867f85c4a787e1d9
SodyFX <38676287+SodyFX@users.noreply.github.com>
2019-12-13 19:56:32 -0800

    Add files via upload

--- sody_clubs.sql
+++ sody_clubs.sql
@@ -14,3 +14,26 @@ CREATE TABLE `sody_clubs_ranks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `users` ADD `club` VARCHAR(30) NULL, ADD `club_rank` TINYINT(5) NULL;
+
+-- LMC EXAMPLE --
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+('club_lmc', 'LMC', 1),
+('club_lmc_black', 'LMC Black', 1),
+('club_lmc_priv', 'LMC Priv', 1),
+('club_lmc_pub', 'LMC Pub', 1);
+
+INSERT INTO `addon_account_data` (`id`, `account_name`, `money`, `owner`) VALUES
+(NULL, 'club_lmc_pub', 0, NULL),
+(NULL, 'club_lmc_priv', 0, NULL),
+(NULL, 'club_lmc_black', 0, NULL),
+(NULL, 'club_lmc', 0, NULL);
+
+INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
+('club_lmc_pub', 'LMC Public', 1),
+('club_lmc_priv', 'LMC Private', 1),
+('club_lmc', 'LMC', 1);
+
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+('club_lmc', 'LMC', 1),
+('club_lmc_priv', 'LMC Priv', 1),
+('club_lmc_pub', 'LMC Pub', 1);
