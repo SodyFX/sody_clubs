09db543d96f7aba51060673e462cd3923c3663f3
SodyFX <38676287+SodyFX@users.noreply.github.com>
2019-12-17 11:30:31 -0800

    Update sody_clubs.sql

--- sody_clubs.sql
+++ sody_clubs.sql
@@ -10,7 +10,8 @@ CREATE TABLE `sody_clubs_ranks` (
   `club_name` varchar(50) NOT NULL,
   `club_rank` tinyint(5) NOT NULL,
   `club_rank_name` varchar(30) NOT NULL,
-  `club_rank_label` varchar(50) NOT NULL
+  `club_rank_label` varchar(50) NOT NULL,
+  `pay` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `users` ADD `club` VARCHAR(30) NULL, ADD `club_rank` TINYINT(5) NULL;
